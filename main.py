87aaefc1d0a561881e91536a10bf35f5824aff96
Klay Clarke <klayaclarke@gmail.com>
2021-11-23 09:30:16 -0500

    create and format the labels and buttons needed to operate this application - no functionality as of yet

--- main.py
+++ main.py
@@ -9,10 +9,30 @@ window.minsize(width=450, height=250)
 user_entry = Entry(width=10)
 user_entry.grid(column=1,row=0)
 
-# label for entry window
+# label to specify measurement for entry window
 
 label_of_miles = Label(text='Miles')
 label_of_miles.grid(column=2,row=0)
+
+# label text 'is equal to'
+
+is_equal_to = Label(text='is equal to')
+is_equal_to.grid(column=0, row=1)
+
+# label to display conversion answer
+
+conversion = Label(text=0)
+conversion.grid(column=1, row=1)
+
+# label to specify measurement for conversion answer
+
+label_of_km = Label(text='KM')
+label_of_km.grid(column=2, row=1)
+
+# button to calculate conversion
+
+calculate = Button(text='calculate')
+calculate.grid(column=1, row=2)
 
 window.mainloop()
 
