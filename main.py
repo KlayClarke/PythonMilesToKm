4dad59b21de422b5634c283fa4d69b0ec91f7985
Klay Clarke <klayaclarke@gmail.com>
2021-11-23 09:49:04 -0500

    add functionality for button press - conversion from user input of miles to km - conversion is rounded to the nearest hundredth

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ window.minsize(width=450, height=250)
 # entry window
 
 user_entry = Entry(width=10)
-user_entry.grid(column=1,row=0)
+user_entry.grid(column=1, row=0)
 
 # label to specify measurement for entry window
 
 label_of_miles = Label(text='Miles')
-label_of_miles.grid(column=2,row=0)
+label_of_miles.grid(column=2, row=0)
 
 # label text 'is equal to'
 
@@ -21,31 +21,29 @@ is_equal_to.grid(column=0, row=1)
 
 # label to display conversion answer
 
-conversion = Label(text=0)
-conversion.grid(column=1, row=1)
+conversion_display = Label(text=0)
+conversion_display.grid(column=1, row=1)
 
 # label to specify measurement for conversion answer
 
 label_of_km = Label(text='KM')
 label_of_km.grid(column=2, row=1)
 
+
+# function to convert user_input to KM
+
+def conversion():
+    user_input = int(user_entry.get())
+    miles_km_conversion = (user_input * 1.609344)
+    conversion_display.config(text=round(miles_km_conversion, 2))
+
+
 # button to calculate conversion
 
-calculate = Button(text='calculate')
+calculate = Button(text='calculate', command=conversion)
 calculate.grid(column=1, row=2)
 
 window.mainloop()
 
-
-
-
-
-
-
-
-#TODO: create entry window where user can specify number of miles they'd desire to convert to km
-#TODO: below that, 'is equal to' then a label that updates with the km conversion 'KM'
-#TODO: a calculate button that commands a function that returns the conversion
-
-
-
+# TODO: a calculate button that commands a function that returns the conversion
+#   by taking user input and converting it to km
